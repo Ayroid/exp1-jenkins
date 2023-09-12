69f90ef2fa2af2eafe0c2eaa78ccce0103788e02
ayroid <ayushsk0000@gmail.com>
2023-09-12 09:47:58 +0530

    modifications

--- src/main/java/mypkg/MyCalculator.java
+++ src/main/java/mypkg/MyCalculator.java
@@ -19,9 +19,8 @@ public class MyCalculator {
 
     public static int power(int a, int b) {
         int result = 1;
-        for (int i = 0; i < b; i++) {
+        for (int i = 0; i < b; i++)
             result *= a;
-        }
         return result;
     }
 
